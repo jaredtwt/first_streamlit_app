2d104e6d6920ca2777b2e4f167a7dd9d1a634fe7
Jared <43511752+jaredtwt@users.noreply.github.com>
2023-08-31 12:02:44 +0800

    Update streamlit_app.py
    change streamlit to st

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-streamlit.head('Breakfast Menu')
+st.head('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
