8ef98af084c0a7c348987f413da2c6f2248aa5ba
Jared <43511752+jaredtwt@users.noreply.github.com>
2023-09-06 10:29:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ if st.button('Get Fruit Load List'):
 def insert_row_snowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
              my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
-             return "Thanks for adding " + new_fruit
+             return "Thanks for adding " + new_fruit + " to the list!! <3"
              
 #text entry box
 try:
